41e9f03e1bec5d82007b8e4b5b2b443846be9a45
Volodymyr Shargorodskyi <gsmkibernetik@gmail.com>
2022-12-29 19:30:55 +0200

    create project with info

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from .views import *
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="home/index.html")),
+    path('info/', TemplateView.as_view(template_name="home/info.html")),
+]
