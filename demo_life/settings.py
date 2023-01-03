2a3696e6bb81002e6fbb74900cebee0365c01dc7
Volodymyr Shargorodskyi <gsmkibernetik@gmail.com>
2023-01-03 19:04:46 +0200

    ready to deploy

--- demo_life/settings.py
+++ demo_life/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jek)u_!jxd!^xhqf&*2j+0cin@88qnyxc493i44nlrk5--_90$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
